1e973615b761b4794c7a2f441ce522b252454c4c
Ricardo Almeida <rcrdalmeida@outlook.pt>
2020-05-27 22:03:42 +0100

    [add] lifecycle methods

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,20 +4,27 @@ import Auxiliary from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
 
-const Modal = (props) => {
-    return (
-        <Auxiliary>
-            <Backdrop show={props.show} click={props.click}></Backdrop>
-            <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-        </Auxiliary>
-    );
+class Modal extends React.Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.show !== nextProps.show;
+    }
+
+    render() {
+        return (
+            <Auxiliary>
+                <Backdrop show={this.props.show} click={this.props.click}></Backdrop>
+                <div 
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Auxiliary>
+        );
+    }
+    
 }
 
 export default Modal;
